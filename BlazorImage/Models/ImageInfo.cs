db8f613554410eebbf2daa744daa31778431c53f
saifsaidi <saifsaidi.jo@hotmail.com>
2025-04-26 21:15:52 +0300

    Remove ToString method from ImageInfo class

--- BlazorImage/Models/ImageInfo.cs
+++ BlazorImage/Models/ImageInfo.cs
@@ -11,10 +11,5 @@
         public string Key { get; set; } = default!;
 
         public DateTime? ProcessedTime { get; set; }
-
-        public override string ToString()
-        {
-            return $"ImageInfo: [SanitizedName={SanitizedName}, Width={Width}, Height={Height}, Format={Format}, Quality={Quality}, ProcessedTime={ProcessedTime}, Key={Key}]";
-        }
     }
 }
